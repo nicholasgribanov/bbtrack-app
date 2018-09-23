912494deaa77b727d676d2c72fb14a065c6fbb66
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-09-23 13:48:56 +0300

    Create multiple maven Spring Project

---
+++ web/src/main/java/com/bbtrack/bbtrack/BbtrackApplication.java
@@ -0,0 +1,12 @@
+package com.bbtrack.bbtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbtrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbtrackApplication.class, args);
+    }
+}
---
+++ web/src/test/java/com/bbtrack/bbtrack/BbtrackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bbtrack.bbtrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BbtrackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
